ec9e177c34720b57264c615fa986288c2f5a887a
Lucas Viegas Milani <l.viegasmilani@gmail.com>
2019-07-28 16:03:16 +0200

    Upgraded Hands
    Further update to Hands

--- include/Hands.hpp
+++ include/Hands.hpp
@@ -1,36 +1,22 @@
-//
-// Created by lucas on 12/06/19.
-//
-
 #ifndef TOUCHLESS_API_HANDS_HPP
 #define TOUCHLESS_API_HANDS_HPP
 
 #include "json.hpp"
 #include "Hand.hpp"
 
-namespace touchless
-{
-class Hands
-{
-public:
-    Hand *leftHand;
-    Hand *rightHand;
+namespace touchless {
+class Hands {
+ public:
+  std::optional<Hand> left_hand_;
+  std::optional<Hand> right_hand_;
 
-    Hands();
-
-    bool equals(Hands *other, double t_distanceTolerance) const;
-
-    double distanceBetweenHandsX() const;
-
-    double distanceBetweenHandsY() const;
-
-    double distanceBetweenHandsZ() const;
-
-    double distanceBetweenHands() const;
-
-    nlohmann::json toJSON();
-
-    void fromJSON(nlohmann::json j);
+  bool Equals(Hands *other, double distance_tolerance) const;
+  [[nodiscard]] double DistanceBetweenHandsX() const;
+  [[nodiscard]] double DistanceBetweenHandsY() const;
+  [[nodiscard]] double DistanceBetweenHandsZ() const;
+  [[nodiscard]] double DistanceBetweenHands() const;
+  [[nodiscard]] nlohmann::json ToJson();
+  void FromJson(nlohmann::json j);
 };
 } // namespace touchless
 
--- src/Hands.cpp
+++ src/Hands.cpp
@@ -1,112 +1,79 @@
-//
-// Created by lucas on 12/06/19.
-//
-
-#include <cstdio>
-#include <cstdlib>
 #include <cmath>
+
 #include "Hands.hpp"
 
-touchless::Hands::Hands()
-{
-    leftHand = nullptr;
-    rightHand = nullptr;
+double touchless::Hands::DistanceBetweenHandsX() const {
+  if (left_hand_ && right_hand_) {
+    return fabs(left_hand_->position_x - right_hand_->position_x);
+  }
+  return -1;
 }
 
-double touchless::Hands::distanceBetweenHandsX() const
-{
-    if (leftHand != nullptr && rightHand != nullptr)
-    {
-        return abs(leftHand->position_x - rightHand->position_x);
-    }
-    return -1;
+double touchless::Hands::DistanceBetweenHandsY() const {
+  if (left_hand_ && right_hand_) {
+    return fabs(left_hand_->position_y - right_hand_->position_y);
+  }
+  return -1;
 }
 
-double touchless::Hands::distanceBetweenHandsY() const
-{
-    if (leftHand != nullptr && rightHand != nullptr)
-    {
-        return abs(leftHand->position_y - rightHand->position_y);
-    }
-    return -1;
+double touchless::Hands::DistanceBetweenHandsZ() const {
+  if (left_hand_ && right_hand_) {
+    return fabs(left_hand_->position_z - left_hand_->position_z);
+  }
+  return -1;
 }
 
-double touchless::Hands::distanceBetweenHandsZ() const
-{
-    if (leftHand != nullptr && rightHand != nullptr)
-    {
-        return abs(leftHand->position_z - leftHand->position_z);
-    }
-    return -1;
+double touchless::Hands::DistanceBetweenHands() const {
+  if (left_hand_ && right_hand_) {
+    return sqrt(pow((left_hand_->position_x - right_hand_->position_x), 2) +
+        pow((left_hand_->position_y - right_hand_->position_y), 2) +
+        pow((left_hand_->position_z - right_hand_->position_z), 2));
+  }
+  return -1;
 }
 
-double touchless::Hands::distanceBetweenHands() const
-{
-    if (leftHand != nullptr && rightHand != nullptr)
-    {
-        return sqrt(pow((leftHand->position_x - rightHand->position_x), 2) +
-                    pow((leftHand->position_y - rightHand->position_y), 2) +
-                    pow((leftHand->position_z - rightHand->position_z), 2));
-    }
-    return -1;
+bool touchless::Hands::Equals(Hands *other, double distance_tolerance) const {
+  distance_tolerance /= 2;
+  return left_hand_ == other->left_hand_ &&
+      right_hand_ == other->right_hand_ &&
+
+      (other->DistanceBetweenHandsX() - distance_tolerance) < DistanceBetweenHandsX() &&
+      (other->DistanceBetweenHandsX() + distance_tolerance) > DistanceBetweenHandsX() &&
+
+      (other->DistanceBetweenHandsY() - distance_tolerance) < DistanceBetweenHandsY() &&
+      (other->DistanceBetweenHandsY() + distance_tolerance) > DistanceBetweenHandsY() &&
+
+      (other->DistanceBetweenHandsZ() - distance_tolerance) < DistanceBetweenHandsZ() &&
+      (other->DistanceBetweenHandsZ() + distance_tolerance) > DistanceBetweenHandsZ();
 }
 
-bool touchless::Hands::equals(Hands *other, double t_distanceTolerance) const
-{
-    double distanceTolerance = t_distanceTolerance / 2;
-    return leftHand == other->leftHand &&
-           rightHand == other->rightHand &&
-
-           (other->distanceBetweenHandsX() - distanceTolerance) < distanceBetweenHandsX() &&
-           (other->distanceBetweenHandsX() + distanceTolerance) > distanceBetweenHandsX() &&
-
-           (other->distanceBetweenHandsY() - distanceTolerance) < distanceBetweenHandsY() &&
-           (other->distanceBetweenHandsY() + distanceTolerance) > distanceBetweenHandsY() &&
-
-           (other->distanceBetweenHandsZ() - distanceTolerance) < distanceBetweenHandsZ() &&
-           (other->distanceBetweenHandsZ() + distanceTolerance) > distanceBetweenHandsZ();
+nlohmann::json touchless::Hands::ToJson() {
+  nlohmann::json j;
+  if (left_hand_) {
+    j["left"] = left_hand_->toJSON();
+  }
+  if (right_hand_) {
+    j["right"] = right_hand_->toJSON();
+  }
+  return j;
 }
 
-nlohmann::json touchless::Hands::toJSON()
-{
-    nlohmann::json j;
-    if (leftHand != nullptr)
-    {
-        j["left"] = leftHand->toJSON();
+void touchless::Hands::FromJson(nlohmann::json j) {
+  if (j.find("left") != j.end()) {
+    if (!left_hand_) {
+      left_hand_ = std::make_optional<Hand>();
     }
-    if (rightHand != nullptr)
-    {
-        j["right"] = rightHand->toJSON();
-    }
-    return j;
-}
+    left_hand_->fromJSON(j["left"]);
+  } else {
+    left_hand_.reset();
+  }
 
-void touchless::Hands::fromJSON(nlohmann::json j)
-{
-    if (j.find("left") != j.end())
-    {
-        if (leftHand == nullptr)
-        {
-            leftHand = new Hand();
-        }
-        leftHand->fromJSON(j["left"]);
-    } else
-    {
-        delete(leftHand);
-        leftHand = nullptr;
+  if (j.find("right") != j.end()) {
+    if (!right_hand_) {
+      right_hand_ = std::make_optional<Hand>();
     }
-
-    if (j.find("right") != j.end())
-    {
-        if (rightHand == nullptr)
-        {
-            rightHand = new Hand();
-        }
-        rightHand->fromJSON(j["right"]);
-    }
-    else
-    {
-        delete(rightHand);
-        rightHand = nullptr;
-    }
+    right_hand_->fromJSON(j["right"]);
+  } else {
+    right_hand_.reset();
+  }
 }
